b003297cb1296d87ccbd6d281f1690e9845ae057
FlamingFury0715 <45069605+FlamingFury0715@users.noreply.github.com>
2021-01-20 11:41:25 -0500

    Initial commit

---
+++ Project_With_Phoenix-main/StoryGameNameLater/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSpeed = 100f;
+    public Transform playerBody;
+    public float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -90f, 35f);
+
+        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Project_With_Phoenix-main/StoryGameNameLater/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 10f;
+    public float normalSpeed = 10f;
+    public float gravity = -10f;
+    public float jumpHeight = 5f;
+    public float sprintMod = 2;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public Vector3 velocity;
+    public bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && isGrounded && normalSpeed == speed)
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetKeyUp(KeyCode.R) && isGrounded && normalSpeed == speed / sprintMod)
+        {
+            speed /= sprintMod;
+        }
+
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
